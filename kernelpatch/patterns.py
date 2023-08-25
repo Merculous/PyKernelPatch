9795b8d20130bff495f911e003a410a3d2ec5325
Merculous <24739823+Merculous@users.noreply.github.com>
2023-08-24 22:36:06 -0500

    Move all of the instructions and their hex values into Pattern as a dictionary.
    Introduce a simple lookup function for getting instructions.
    
    This makes organization much better. Also I believe makes the code a tad faster?
    
    If we need to reference the instructions that we use in pattern finding, we can just reference them via the Pattern object.

--- kernelpatch/patterns.py
+++ kernelpatch/patterns.py
@@ -1,170 +1,186 @@
 
-ldr_r2_r4_x28 = b'\xa2\x6a'
-
-ldr_r3_r3 = b'\x1b\x68'
-
-mov_r3_1 = b'\x01\x23'
-
-mov_r1_r5 = b'\x29\x46'
-
-movs_r2_x13 = b'\x13\x22'
-
-blx_r10 = b'\xd0\x47'
-
-movs_r1_1 = b'\x01\x21'
-
-cbz_r0_x14 = b'\x40\xb1'
-
-adds_r5_x13 = b'\x13\x35'
-
-movs_r2_1 = b'\x01\x22'
-
-strw_r8_sp = b'\xcd\xf8\x00\x80'
-
-strw_r8_sp_4 = b'\xcd\xf8\x04\x80'
-
-str_r3_sp_8 = b'\x02\x93'
-
-blx_r12 = b'\xe0\x47'
-
-strw_r8_sp_8 = b'\xcd\xf8\x08\x80'
-
-cmp_r0_0 = b'\x00\x28'
-
-movs_r1_2 = b'\x02\x21'
-
-ldr_r4_pc_x214 = b'\x85\x4c'
-
-blx_r4 = b'\xa0\x47'
-
-movs_r0_0 = b'\x00\x20'
-
-bne_to_movw_r6_x2e2 = b'\xf2\xd1'  # 40 F2 E2 26
-
-ldr_r0_sp_x14 = b'\x05\x98'
-
-ldr_r4_pc_x20c = b'\x83\x4c'
-
-ldr_r6_pc_xac = b'\x2b\x4e'
-
-mov_r0_r5 = b'\x28\x46'
-
-ldr_r1_sp_8 = b'\x02\x99'
-
-blx_r6 = b'\xb0\x47'
-
-subw_r4_r7_x18 = b'\xa7\xf1\x18\x04'
-
-mov_r0_r1 = b'\x08\x46'
-
-mov_sp_r4 = b'\xa5\x46'
-
-cbnz_r0_x7c = b'\xc0\xbb'
-
-it_ne = b'\x18\xbf'
-
-movnew_r8_1 = b'\x4f\xf0\x01\x08'
-
-mov_r0_r8 = b'\x40\x46'
-
-add_sp_sp_x14 = b'\x05\xb0'
-
-bne_to_movw_r6_x2e2_second = b'\xed\xd1'
-
-movw_r1_neg1 = b'\x4f\xf0\xff\x31'  # MOVEQ.W         R1, #0xFFFFFFFF
-
-popw_r8_r10_r11 = b'\xbd\xe8\x00\x0d'
-
-cmp_r0_6 = b'\x06\x28'
-
-andw_r0_r1_6 = b'\x01\xf0\x06\x00'
-
-str_r1_sp_4 = b'\x01\x91'
-
-bne_x16 = b'\x06\xd1'
-
-blx_r2 = b'\x90\x47'
-
-b_x23a = b'\x19\xe1'
-
-cbz_r0_x12 = b'\x38\xb1'
-
-ldr_r1_pc_x14 = b'\x05\x49'
-
-ldr_r1_r1 = b'\x09\x68'
-
-ldr_r0_pc_4 = b'\x01\x48'
-
-ldr_r0_r0 = b'\x00\x68'
-
-bx_lr = b'\x70\x47'
-
-str_r1_r0 = b'\x01\x60'
-
-
 class Pattern:
+    instructions = {
+        'ldr': {
+            'ldr_r2_r4_x28': b'\xa2\x6a',
+
+            'ldr_r3_r3': b'\x1b\x68',
+
+            'ldr_r4_pc_x214': b'\x85\x4c',
+
+            'ldr_r0_sp_x14': b'\x05\x98',
+
+            'ldr_r4_pc_x20c': b'\x83\x4c',
+
+            'ldr_r6_pc_xac': b'\x2b\x4e',
+
+            'ldr_r1_sp_8': b'\x02\x99',
+
+            'ldr_r1_pc_x14': b'\x05\x49',
+
+            'ldr_r1_r1': b'\x09\x68',
+
+            'ldr_r0_pc_4': b'\x01\x48',
+
+            'ldr_r0_r0': b'\x00\x68',
+        },
+        'mov': {
+            'mov_r3_1': b'\x01\x23',
+
+            'mov_r1_r5': b'\x29\x46',
+
+            'mov_r0_r5': b'\x28\x46',
+
+            'mov_r0_r1': b'\x08\x46',
+
+            'mov_sp_r4': b'\xa5\x46',
+
+            'mov_r0_r8': b'\x40\x46',
+
+            'movw_r1_neg1': b'\x4f\xf0\xff\x31',
+
+            'movnew_r8_1': b'\x4f\xf0\x01\x08',
+
+            'movs_r0_0': b'\x00\x20',
+
+            'movs_r1_2': b'\x02\x21',
+
+            'movs_r2_1': b'\x01\x22',
+
+            'movs_r1_1': b'\x01\x21',
+
+            'movs_r2_x13': b'\x13\x22'
+        },
+        'cmp': {
+            'cmp_r0_0': b'\x00\x28',
+
+            'cmp_r0_6': b'\x06\x28'
+        },
+        'str': {
+            'str_r3_sp_8': b'\x02\x93',
+
+            'str_r1_sp_4': b'\x01\x91',
+
+            'str_r1_r0': b'\x01\x60',
+
+            'strw_r8_sp_8': b'\xcd\xf8\x08\x80',
+
+            'strw_r8_sp': b'\xcd\xf8\x00\x80',
+
+            'strw_r8_sp_4': b'\xcd\xf8\x04\x80'
+        },
+        'and': {
+            'andw_r0_r1_6': b'\x01\xf0\x06\x00'
+        },
+        'pop': {
+            'popw_r8_r10_r11': b'\xbd\xe8\x00\x0d'
+        },
+        'add': {
+            'add_sp_sp_x14': b'\x05\xb0',
+            'adds_r5_x13': b'\x13\x35'
+        },
+        'it': {
+            'it_ne': b'\x18\xbf'
+        },
+        'sub': {
+            'subw_r4_r7_x18': b'\xa7\xf1\x18\x04'
+        },
+        'b': {
+            'b_x23a': b'\x19\xe1',
+
+            'bx_lr': b'\x70\x47',
+
+            'cbz_r0_x12': b'\x38\xb1',
+
+            'blx_r2': b'\x90\x47',
+
+            'bne_x16': b'\x06\xd1',
+
+            'bne_to_movw_r6_x2e2_second': b'\xed\xd1',
+
+            'cbnz_r0_x7c': b'\xc0\xbb',
+
+            'blx_r6': b'\xb0\x47',
+
+            'bne_to_movw_r6_x2e2': b'\xf2\xd1',  # 40 F2 E2 26
+
+            'blx_r4': b'\xa0\x47',
+
+            'blx_r12': b'\xe0\x47',
+
+            'blx_r10': b'\xd0\x47',
+
+            'cbz_r0_x14': b'\x40\xb1'
+        }
+    }
+
     def __init__(self, version):
         self.version = version
 
+    def getHex(self, instruction):
+        for base in self.instructions:
+            if instruction in self.instructions[base]:
+                return self.instructions[base][instruction]
+
     def form_CSEnforcement(self):
         pattern = (
-            ldr_r2_r4_x28,
-            ldr_r3_r3
+            self.getHex('ldr_r2_r4_x28'),
+            self.getHex('ldr_r3_r3')
         )
         return (b''.join(pattern),)
 
     def form_AMFIMemcmp(self):
         if self.version == '5.0':
             pattern = (
-                mov_r1_r5,
-                movs_r2_x13,
-                blx_r10,
-                movs_r1_1
+                self.getHex('mov_r1_r5'),
+                self.getHex('movs_r2_x13'),
+                self.getHex('blx_r10'),
+                self.getHex('movs_r1_1')
             )
             return (b''.join(pattern),)
 
         elif self.version == '5.0.1':
             pattern = (
-                mov_r1_r5,
-                movs_r2_x13,
-                blx_r10,
+                self.getHex('mov_r1_r5'),
+                self.getHex('movs_r2_x13'),
+                self.getHex('blx_r10'),
                 b'\x01'
             )
             return (b''.join(pattern),)
 
         elif self.version == '5.1':
             pattern = (
-                mov_r1_r5,
-                movs_r2_x13,
-                blx_r10,
-                movs_r1_1,
-                cbz_r0_x14,
-                adds_r5_x13,
+                self.getHex('mov_r1_r5'),
+                self.getHex('movs_r2_x13'),
+                self.getHex('blx_r10'),
+                self.getHex('movs_r1_1'),
+                self.getHex('cbz_r0_x14'),
+                self.getHex('adds_r5_x13'),
                 b'\x00'
             )
             return (b''.join(pattern),)
 
         elif self.version == '5.1.1':
             pattern = (
-                mov_r1_r5,
-                movs_r2_x13,
-                blx_r10
+                self.getHex('mov_r1_r5'),
+                self.getHex('movs_r2_x13'),
+                self.getHex('blx_r10')
             )
             return (b''.join(pattern),)
 
     def form_PE_i_can_has_debugger(self):
         if self.version == '5.1.1':
             pattern1 = (
-                cbz_r0_x12,
-                ldr_r1_pc_x14,
-                ldr_r1_r1
+                self.getHex('cbz_r0_x12'),
+                self.getHex('ldr_r1_pc_x14'),
+                self.getHex('ldr_r1_r1')
             )
 
             pattern2 = (
-                str_r1_r0,
-                ldr_r0_pc_4,
-                ldr_r0_r0,
-                bx_lr
+                self.getHex('str_r1_r0'),
+                self.getHex('ldr_r0_pc_4'),
+                self.getHex('ldr_r0_r0'),
+                self.getHex('bx_lr')
             )
 
             return (b''.join(pattern1), b''.join(pattern2))
@@ -172,35 +188,35 @@ class Pattern:
     def form_AppleImage3NORAccess(self):
         if self.version == '5.1.1':
             pattern1 = (
-                str_r3_sp_8,
-                blx_r12,
-                cbnz_r0_x7c
+                self.getHex('str_r3_sp_8'),
+                self.getHex('blx_r12'),
+                self.getHex('cbnz_r0_x7c')
             )
 
             pattern2 = (
-                strw_r8_sp_4,
-                strw_r8_sp_8,
-                blx_r12,
-                cmp_r0_0
+                self.getHex('strw_r8_sp_4'),
+                self.getHex('strw_r8_sp_8'),
+                self.getHex('blx_r12'),
+                self.getHex('cmp_r0_0')
             )
 
             pattern3 = (
-                ldr_r4_pc_x214,
-                blx_r4,
-                cmp_r0_0,
-                bne_to_movw_r6_x2e2
+                self.getHex('ldr_r4_pc_x214'),
+                self.getHex('blx_r4'),
+                self.getHex('cmp_r0_0'),
+                self.getHex('bne_to_movw_r6_x2e2')
             )
 
             pattern4 = (
-                ldr_r4_pc_x20c,
-                blx_r4,
-                cmp_r0_0
+                self.getHex('ldr_r4_pc_x20c'),
+                self.getHex('blx_r4'),
+                self.getHex('cmp_r0_0')
             )
 
             pattern5 = (
-                ldr_r1_sp_8,
-                blx_r6,
-                cmp_r0_0
+                self.getHex('ldr_r1_sp_8'),
+                self.getHex('blx_r6'),
+                self.getHex('cmp_r0_0')
             )
 
             patterns = (
@@ -216,21 +232,21 @@ class Pattern:
     def form_signatureCheck(self):
         if self.version == '5.0' or self.version == '5.0.1' or self.version == '5.1':
             pattern = (
-                movw_r1_neg1,
-                subw_r4_r7_x18,
-                mov_r0_r1,
-                mov_sp_r4,
-                popw_r8_r10_r11,
+                self.getHex('movw_r1_neg1'),
+                self.getHex('subw_r4_r7_x18'),
+                self.getHex('mov_r0_r1'),
+                self.getHex('mov_sp_r4'),
+                self.getHex('popw_r8_r10_r11'),
                 b'\xf0'
             )
 
         else:
             pattern = (
-                movw_r1_neg1,
-                subw_r4_r7_x18,
-                mov_r0_r1,
-                mov_sp_r4,
-                popw_r8_r10_r11
+                self.getHex('movw_r1_neg1'),
+                self.getHex('subw_r4_r7_x18'),
+                self.getHex('mov_r0_r1'),
+                self.getHex('mov_sp_r4'),
+                self.getHex('popw_r8_r10_r11')
             )
 
             return (b''.join(pattern),)
@@ -238,8 +254,8 @@ class Pattern:
     def form_vm_map_enter(self):
         if self.version == '5.1.1':
             pattern = (
-                andw_r0_r1_6,
-                cmp_r0_6
+                self.getHex('andw_r0_r1_6'),
+                self.getHex('cmp_r0_6')
             )
 
             return (b''.join(pattern),)
@@ -253,8 +269,8 @@ class Pattern:
     def form_tfp0(self):
         if self.version == '5.1.1':
             pattern = (
-                str_r1_sp_4,
-                bne_x16
+                self.getHex('str_r1_sp_4'),
+                self.getHex('bne_x16')
             )
 
             return (b''.join(pattern),)
@@ -274,9 +290,9 @@ class Pattern:
     def form_AMFIHook(self):
         if self.version == '5.1.1':
             pattern = (
-                mov_r0_r5,
-                blx_r2,
-                b_x23a
+                self.getHex('mov_r0_r5'),
+                self.getHex('blx_r2'),
+                self.getHex('b_x23a')
             )
 
             return (b''.join(pattern),)
